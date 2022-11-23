485bc2c93c9a9228663032263e8f946e29cc284a
Vitor dos Santos <vdossan1@my.westga.edu>
2022-11-23 17:44:48 -0500

    Implement GUI
    1)GUI created but elements have no id or inAction.

--- BulletinBoard/src/edu/westga/cs1302/bulletin_board/view/MainWindow.java
+++ BulletinBoard/src/edu/westga/cs1302/bulletin_board/view/MainWindow.java
@@ -1,10 +1,22 @@
 package edu.westga.cs1302.bulletin_board.view;
 
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 /** Codebehind for the MainWindow of the Application.
  * 
  * @author CS 1302
  * @version Fall 2022
  */
 public class MainWindow {
+
 
 }
